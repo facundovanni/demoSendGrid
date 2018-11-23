bc0f27e2de01aab870652570950c360dca12c5e3
facundovanni <facundovanni@gmail.com>
2018-11-23 03:00:20 -0300

    demo cesar

---
+++ DemoSendGrid/Controllers/CorreoController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoSendGrid.Controllers
+{
+    public class CorreoController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult EnviarCorreo()
+        {
+            return View();
+        }
+        // TODO 003. Creamos la accion en el controlador para enviar correos.
+        [HttpPost]
+        public ActionResult EnviarCorreo(String CorreoUsuario, String Destino, String Asunto, String Mensaje, String Dni, String Nombre)
+        {
+            try
+            {
+                Asunto = Nombre + " con " + Dni + " quiere saber.....";
+
+                new EnviarCorreos().PostMails(Nombre, CorreoUsuario, Destino, Asunto, Mensaje);
+                //CC} finally {
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                Asunto = "Copia de tu solicitud con fecha" + DateTime.Now.Hour + ":" + DateTime.Now.Minute + " No respondas a este mensaje";
+                Destino = CorreoUsuario;
+                CorreoUsuario = "connecting.us2018@gmail.com";
+                Nombre = "ConnectingUs";
+
+
+             new EnviarCorreos().PostMails(Nombre, CorreoUsuario, Destino, Asunto, Mensaje);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+
+            return RedirectToAction("Index");
+        }
+
+
+
+    }
+}
---
+++ DemoSendGrid/EnviarCorreos.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Web;
+
+namespace DemoSendGrid
+{
+    public class EnviarCorreos
+    {
+        // TODO 002. Creamos un metodo que nos genera el envío de de correos
+        public void PostMails(String Name,String mailFrom,String mailsender, String asunto, String body)
+        {
+            try
+            {
+                MailMessage mailMsg = new MailMessage();
+
+                // To
+                mailMsg.To.Add(new MailAddress(mailsender));
+
+                // From
+                mailMsg.From = new MailAddress(mailFrom, Name);
+
+                // Subject and multipart/alternative Body
+                mailMsg.Subject = asunto;
+                string text = body;
+                string html = body;
+                mailMsg.AlternateViews.Add(AlternateView.CreateAlternateViewFromString(text, null, MediaTypeNames.Text.Plain));
+                mailMsg.AlternateViews.Add(AlternateView.CreateAlternateViewFromString(html, null, MediaTypeNames.Text.Html));
+
+              
+                SmtpClient smtpClient = new SmtpClient("smtp.sendgrid.net", Convert.ToInt32(587));
+             
+
+                smtpClient.Send(mailMsg);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
